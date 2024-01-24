512539ce7bc9f910c20dec5b21562654e8350217
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-01-23 23:38:26 -0500

    Firts Commit App Movies

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/src/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Películas',
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => const HomePage(),
+      },
+    );
+  }
+}
